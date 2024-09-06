79369b96dd964bc3e4c3e1a38f8e9c77e585f995
xingyanpo <xingyanpo@gmail.com>
2024-09-06 17:45:32 +0800

    change 2024年9月6日17:45:30

--- src/app/not-found.js
+++ src/app/not-found.js
@@ -16,7 +16,7 @@ export default function NotFound() {
           <Link href={'/'} className="px-4 inline py-2 text-sm font-medium leading-5 shadow text-white transition-colors duration-150 border border-transparent rounded-lg focus:outline-none focus:shadow-outline-blue bg-blue-600 active:bg-blue-600 hover:bg-blue-700">back to homepage</Link>
         </div>
         <div className="max-w-lg">
-          <Image src="/svg/cat.svg" alt="cat" width={300} height={300}/>
+          <Image src="/svg/cat.svg" alt="cat" width={300} height={300} priority/>
         </div>
 
       </div>
--- src/middleware.js
+++ src/middleware.js
@@ -1,16 +1,22 @@
-import { NextResponse } from 'next/server'
+import JWT from '@/utils/JWT'
 
 export function middleware(request) {
-  if (request.nextUrl.pathname.startsWith('/admin')) {
-    if (!request.nextUrl.pathname.startsWith('/admin/login')) {
-      if (true) {
-        //已经登录什么都不做
-      } else {
-        return NextResponse.redirect(new URL('/admin/login', request.url))
-      }
+  const token = request.headers.get('authorization')?.split(' ')[1];
+  const { pathname } = request.nextUrl;
+
+  if (pathname.startsWith('/admin') && pathname !== '/admin/login') {
+    if (!token) {
+      return new Response('未授权', { status: 403 });
     }
-  };
-  if (request.nextUrl.pathname.startsWith('/api/admin')) {
-    console.log('api/admin')
+    try {
+      const decoded = JWT.verify(token);
+      request.user = decoded;
+    } catch (error) {
+      return new Response('无效的令牌', { status: 401 });
+    }
   }
 }
+
+export const config = {
+  matcher: ['/admin/:path*']
+};
