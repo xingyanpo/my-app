f4b4df2c0be46707b780dc2d44932bc3f163ddf1
xingyanpo <xingyanpo@gmail.com>
2024-09-24 17:46:24 +0800

    change

--- src/components/common/footer-company.js
+++ src/components/common/footer-company.js
@@ -2,6 +2,6 @@ import React from 'react'
 
 export default function FooterCompany() {
   return (
-    <div className='text-left'>Copyright @ {new Date().getFullYear()} RS ENERGY Inc.</div>
+    <div className='text-left'>Copyright @{new Date().getFullYear()} RS ENERGY Inc.</div>
   )
 }
